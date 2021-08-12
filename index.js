ea7b215e0bd6fa39abc07c16213a1f763f11ad6e
Park <cksdud8028@naver.com>
2021-08-12 11:08:49 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,15 +2,38 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+const config = require('./config/key');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://nodejs:<1234>@ptcnodejs.lgiap.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB connected..')).catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('노드제이에스 연습중 ㅋ')
+  res.send('노드제이에스 연습중 ㅋㅋ')
 })
+
+app.post('/register', (req, res) => {
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err});
+    return res.status(200).json({ success: true})
+  })
+
+
+
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
